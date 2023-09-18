ceee486e10d7258b946b53badddf462787af2822
cristivnmrp <c.morales.p95@gmail.com>
2023-09-18 20:57:24 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const pizza = {
+    masa: "",
+    ingredientes: []
+};
+
+pizza.masa = prompt("¿Qué masa de pizza deseas? (Delgada, Tradicional o Integral)");
+
+while (true) {
+    const ingrediente = prompt("Agrega un ingrediente a tu pizza (o escribe 'listo' para terminar):");
+    
+    if (ingrediente.toLowerCase() === "listo") {
+    break;
+    }
+    
+    pizza.ingredientes.push(ingrediente);
+}
+
+let mensajePizza = "¡Aquí está tu pizza!\n";
+mensajePizza += "Masa de pizza: " + pizza.masa + "\n";
+mensajePizza += "Ingredientes:\n";
+for (let i = 0; i < pizza.ingredientes.length; i++) {
+    mensajePizza += "- " + pizza.ingredientes[i] + "\n";
+}
+
+alert(mensajePizza);
