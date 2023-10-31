28bd9ba6f3209e34d6ee60afe78537e33f9a055b
cristivnmrp <c.morales.p95@gmail.com>
2023-10-30 21:11:34 -0300

    n

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const pizza = {
-    masa: "",
-    ingredientes: []
-};
+const pizzaForm = document.getElementById('pizza-form');
+const ingredienteInput = document.getElementById('ingrediente');
+const cantidadInput = document.getElementById('cantidad');
+const ingredientesList = document.getElementById('ingredientes-list');
+const totalElement = document.getElementById('total');
+const agregarButton = document.getElementById('agregar');
 
-const pizzaJSON = JSON.stringify(pizza);
+let total = 0;
+const ingredientes = [];
 
-const pizzaInfoElement = document.getElementById("pizzaInfo");
-
-const pizzaObj = JSON.parse(pizzaJSON);
-
-// Definir precios de los ingredientes
 const preciosIngredientes = {
     pepperoni: 2.5,
     champiñones: 1.5,
@@ -21,45 +19,42 @@ const preciosIngredientes = {
     tomate: 1.0,
     carne: 2.0,
     pollo: 2.0,
-    extraqueso: 2.0,
+    extraqueso: 2.0
 };
 
-const pizza1 = {
-    masa: prompt("Hola, bienvenido a Crea tu pizza ¿Qué masa de pizza deseas? (Delgada, Tradicional o Integral)"),
-    ingredientes: [],
-};
+agregarButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    const ingrediente = ingredienteInput.value;
+    const cantidad = parseInt(cantidadInput.value);
 
-let total = 0;
+    if (ingrediente && preciosIngredientes[ingrediente] && !isNaN(cantidad) && cantidad > 0) {
+        const precioIngrediente = preciosIngredientes[ingrediente];
+        const costoIngrediente = precioIngrediente * cantidad;
 
-while (true) {
-    const ingrediente = prompt("Agrega uno o más ingredientes a tu pizza (o escribe 'listo' para terminar), los ingredientes y sus valores son los siguientes:  pepperoni $2.5, champiñones $1.5, jamón $2.0, pimientos $1.0, cebolla $1.0, aceitunas $1.5, choclo $1.0, tomate $1.0, carne $2.0, pollo $2.0, y extraqueso $2.0");
-
-    if (ingrediente.toLowerCase() === "listo") {
-        break;
-    }
-
-    const cantidad = parseInt(prompt(`¿Cuánta cantidad de ${ingrediente} deseas agregar?`));
-    const precioIngrediente = preciosIngredientes[ingrediente.toLowerCase()] || 0;
-    const costoIngrediente = precioIngrediente * (isNaN(cantidad) ? 0 : cantidad);
-
-    if (costoIngrediente > 0) {
-        pizza.ingredientes.push({ ingrediente, cantidad, costoIngrediente });
         total += costoIngrediente;
+        ingredientes.push({ ingrediente, cantidad, costoIngrediente });
+
+        const li = document.createElement('li');
+        li.textContent = `${cantidad} ${ingrediente} - $${costoIngrediente.toFixed(2)}`;
+        ingredientesList.appendChild(li);
+
+        totalElement.textContent = `Total: $${total.toFixed(2)}`;
+
+        ingredienteInput.value = '';
+        cantidadInput.value = '';
     } else {
-        alert("El ingrediente ingresado no está en la lista de opciones.");
+        alert('Por favor, selecciona un ingrediente válido y proporciona una cantidad válida.');
     }
-}
+});
 
-const { masa, ingredientes } = pizza;
+pizzaForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const masa = document.getElementById('masa').value;
 
-const mensajePizza = `¡Aquí está tu pizza!\nMasa de pizza: ${masa}\nIngredientes:\n${ingredientes.map(ing => `- ${ing.cantidad} ${ing.ingrediente} - $${ing.costoIngrediente.toFixed(2)}`).join('\n')}\nEl valor de tu pizza es: $${total.toFixed(2)}`;
-alert(mensajePizza);
+    const mensajePizza = `¡Aquí está tu pizza!\nMasa de pizza: ${masa}\nIngredientes:\n${ingredientes.map(ingrediente => `- ${ingrediente.cantidad} ${ingrediente.ingrediente} - $${ingrediente.costoIngrediente.toFixed(2)}`).join('\n')}\nTotal: $${total.toFixed(2)}`;
+    
+    alert(mensajePizza);
+});
 
-mensajePizza += "Ingredientes:\n";
-for (let i = 0; i < pizza.ingredientes.length; i++) {
-    mensajePizza += `- ${pizza.ingredientes[i].cantidad} ${pizza.ingredientes[i].ingrediente} - $${pizza.ingredientes[i].costoIngrediente.toFixed(2)}\n`;
-}
-
-mensajePizza += `El valor de tu pizza es: $${total.toFixed(2)}`;
-
-pizzaInfoElement.textContent = mensajePizza;
